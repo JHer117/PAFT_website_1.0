84d3eb5cfff967816b36af47b055794ac8b1540b
Snacks <whiteintheiris@gmail.com>
2020-11-20 03:51:01 -0800

    added scrolling betwix section links

--- static/media.js
+++ static/media.js
@@ -11,7 +11,39 @@ $(document).ready(function(){
         });
     });
 
+    document.querySelectorAll('a[href^="#packages_link"]').forEach($anchor => {
+        $anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
 
+
+    document.querySelectorAll('a[href^="#about_section"]').forEach($anchor => {
+        $anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
+
+
+    document.querySelectorAll('a[href^="#contact"]').forEach($anchor => {
+        $anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
+
+   
 
 
 
