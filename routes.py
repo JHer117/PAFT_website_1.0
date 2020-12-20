66c98c2052a02a87460ecea25b00b4a4cf1a0637
Snacks <whiteintheiris@gmail.com>
2020-12-20 00:40:13 -0800

    it   is    finished

--- routes.py
+++ routes.py
@@ -2,5 +2,5 @@ from config import app
 from controller_functions import landing, about_page, packages_page
 
 app.add_url_rule("/", view_func=landing)
-app.add_url_rule("/about", view_func=about_page)
-app.add_url_rule("/packages", view_func=packages_page)
+app.add_url_rule("/about.html", view_func=about_page)
+app.add_url_rule("/packages.html", view_func=packages_page)
