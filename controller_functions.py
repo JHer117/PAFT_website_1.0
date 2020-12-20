dee003796111147e43816c22d100d34748ab8d02
Snacks <whiteintheiris@gmail.com>
2020-12-19 23:29:02 -0800

    added formspree contact and finished python routes

--- controller_functions.py
+++ controller_functions.py
@@ -7,8 +7,13 @@ import re
 
 # Home Page
 def landing():
-    
-    return render_template("index.htm")
+    return render_template("index.html")
+#About page
+def about_page():
+    return render_template("about.html")
+#Packages page
+def packages_page():
+    return render_template("packages.html")
 
 
 
